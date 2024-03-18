65e945e6051e4122e273c6ccbc4828203fdb2c5e
M Sai Preethi <122619685+saipreethi-msp@users.noreply.github.com>
2024-03-18 23:36:43 +0530

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -1,7 +1,7 @@
-import streamlit as st 
-import pandas_profiling
 import plotly.express as px
 import pandas as pd 
+import pandas_profiling
+import streamlit as st 
 
 
 from streamlit_pandas_profiling import st_profile_report
